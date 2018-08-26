3219845d2432b7cf8a41de80f08b87148ea11e5e
Chronis Panagiotidis <panagiotidisxronis@gmail.com>
2018-08-26 12:43:21 +0200

    Fix Travis CI error of importing custome module to test file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Panagiotidis Chronis',
+      author_email='panagiotidisxronis@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command']
+      )
