001ca7494bb8cca6b820861490ea07e125194f55
Chronis Panagiotidis <panagiotidisxronis@gmail.com>
2018-09-17 13:02:28 +0200

    Saving PIL images to temporary folder as npy files

--- scripts/image_loader_using_pykitti.py
+++ scripts/image_loader_using_pykitti.py
@@ -1,5 +1,6 @@
 import os
 import time
+import tempfile
 
 import pykitti
 import numpy as np
@@ -29,7 +30,7 @@ def _load_images(basedir, date, drive):
     return [image for image in data.cam2]
 
 
-def _transfort_images_to_npy_files(basedir, date, drive):
+def _transfort_images_to_npy_files(temp_file, basedir, date, drive):
     # The 'frames' argument is optional - default: None, which loads the whole dataset.
     # Calibration, timestamps, and IMU data are read automatically.
     # Camera and velodyne data are available via properties that create generators
@@ -48,8 +49,10 @@ def _transfort_images_to_npy_files(basedir, date, drive):
     # dataset.velo:          Returns a generator that loads velodyne scans as [x,y,z,reflectance]
     # dataset.get_velo(idx): Returns the velodyne scan at idx
 
+    temp_file_path = temp_file + '\{}'.format(np.random.randint(1, 1000000))
+
     for image in data.cam2:
-        np.save('file_{}'.format(np.random.randint(1, 1000000)), np.array(image))
+        np.save(temp_file_path, np.array(image))
 
 
 def load_images(date='2011_09_26', basedir='E:\Documents\KITTI\Raw'):
@@ -65,7 +68,7 @@ def load_images(date='2011_09_26', basedir='E:\Documents\KITTI\Raw'):
     return images
 
 
-def transform_images(date='2011_09_26', basedir='E:\Documents\KITTI\Raw'):
+def transform_images(temp_file, date='2011_09_26', basedir='E:\Documents\KITTI\Raw'):
     folder = basedir+'\\{}'.format(date)
     drives = [name for name in os.listdir(folder) if os.path.isdir(folder+'\\{}'.format(name))]
 
@@ -73,12 +76,14 @@ def transform_images(date='2011_09_26', basedir='E:\Documents\KITTI\Raw'):
 
     for drive in drives:
         drive = drive.split('_')[-2]
-        _transfort_images_to_npy_files(basedir, date, drive)
+        _transfort_images_to_npy_files(temp_file, basedir, date, drive)
 
     return images
 
 
 if __name__ == '__main__':
+
+    temp_folder = tempfile.mkdtemp()
 
     start_time = time.time()
     # images = load_images()
@@ -87,6 +92,7 @@ if __name__ == '__main__':
     #     np.save('file_{}'.format(np.random.randint(0, 100)), np.array(image))
     #     time.sleep(3)
 
-    transform_images()
+    transform_images(temp_file=temp_folder, basedir='C:\\Users\\ppanagiotidis\\Pictures\\Raw')
 
+    print(temp_folder)
     print('Execution Time: {} secs'.format(time.time() - start_time))
